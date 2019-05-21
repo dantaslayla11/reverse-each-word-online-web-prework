7a7576d8fe99bda9775d182513fdb2a61cf1d3bb
layla dantas <laylatdantas@gmail.com>
2019-05-21 20:10:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
    string1 = sentence1.split(" ")
-   array = string1.collect
-   puts array
+   array = string1.collect {|item| item.reverse}
+   puts array   
    #string1.each do |item|
     # array.push(item.reverse)
     # string1.collect
