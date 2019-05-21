52ce28695e8e492c238fe6c45fe603dbf3166dd6
layla dantas <laylatdantas@gmail.com>
2019-05-21 20:00:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
    string1 = sentence1.split(" ")
-   string1.collect do |item|
+   string1.each do |item|
      string1 = item.reverse
    end
    string1.join(" ")
